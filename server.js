d372921b65bef41768afc7d50f70fae0be29da5f
K-Mannnn <67024458+K-Mannnn@users.noreply.github.com>
2022-02-09 21:09:13 +0000

    new

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const path = require('path');
 //  app.use(express.static(path.join(__dirname, '../client/src/images')));
  
  app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname + '../client/src/index.js'));
+   res.sendFile(path.join(__dirname + './client/build/static'));
  });
 
 
